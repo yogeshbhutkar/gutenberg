c734a081dc2f2a7a2960fc9d8b5f825d288b7faf
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2024-12-30 10:21:14 +0530

    i18n: make example translatable in comments-pagination-previous

--- packages/block-library/src/comments-pagination-previous/index.js
+++ packages/block-library/src/comments-pagination-previous/index.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { _x } from '@wordpress/i18n';
 import { queryPaginationPrevious as icon } from '@wordpress/icons';
 
 /**
@@ -16,6 +17,14 @@ export { metadata, name };
 export const settings = {
 	icon,
 	edit,
+	example: {
+		attributes: {
+			label: _x(
+				'Comments Previous Page',
+				'Example label for the Comments Pagination Previous block'
+			),
+		},
+	},
 };
 
 export const init = () => initBlock( { name, metadata, settings } );

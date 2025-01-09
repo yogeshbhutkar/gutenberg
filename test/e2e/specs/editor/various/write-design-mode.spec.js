dca507ca2e33f5df3cb467f18560a860e92d4aa6
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-01-09 09:38:11 +0530

    Test: Update options menu count to reflect new item addition

--- test/e2e/specs/editor/various/write-design-mode.spec.js
+++ test/e2e/specs/editor/various/write-design-mode.spec.js
@@ -108,8 +108,8 @@ test.describe( 'Write/Design mode', () => {
 			.getByRole( 'menu', { name: 'Options' } )
 			.getByRole( 'menuitem' );
 
-		// we expect 3 items in the options menu
-		await expect( optionsMenu ).toHaveCount( 3 );
+		// we expect 4 items in the options menu
+		await expect( optionsMenu ).toHaveCount( 4 );
 
 		// We should be able to select the paragraph block and write in it.
 		await paragraph.click();

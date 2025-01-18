05863122b0fe313a004a85ea060c3366bb3a4901
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2025-01-18 15:53:21 +0900

    Search Results Title Block: Add dropdown menu props to Tools Panel component (#68763)
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>

--- packages/block-library/src/query-title/edit.js
+++ packages/block-library/src/query-title/edit.js
@@ -141,6 +141,7 @@ export default function QueryTitleEdit( {
 								showSearchTerm: true,
 							} )
 						}
+						dropdownMenuProps={ dropdownMenuProps }
 					>
 						<ToolsPanelItem
 							hasValue={ () => ! showSearchTerm }
